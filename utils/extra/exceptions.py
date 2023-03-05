d85fec53869ed63230bf68e7233662e63a8f357c
Lia Marie <lia.thaila.students@gmail.com>
2023-03-05 23:46:53 +0100

    Customizable formatting for UserFeedbackException

--- utils/extra/exceptions.py
+++ utils/extra/exceptions.py
@@ -1,5 +1,6 @@
 from __future__ import annotations
 
+from typing import Callable
 from abc import ABC, abstractmethod
 from enum import Enum
 
@@ -16,13 +17,59 @@ class ExceptionLevel(Enum):
 
 
 class UserFeedbackException(Exception):
-    def __init__(self, message: str, emoji_strategy: UserFeedbackEmojiStrategy) -> None:
+    """Base class for exceptions that are meant to be shown to the user.
+    
+    Meant to be used with the `:class:UserFeedbackExceptionFactory`.
+
+    Notes
+    -----
+    Formatters can be found in `utils/extra/helper.py` and are used to format the
+    message before it is shown to the user.
+
+    - `bold` - **bold**
+    - `underline` - __underline__
+    - `quote` - > quote
+    - `codeblock` - `codeblock`
+
+    EmojiStrategies
+    ---------------
+    The emoji strategy is used to determine what emoji should be shown before the
+
+    - `ErrorUserFeedbackEmoji` - ❌
+    - `WarningUserFeedbackEmoji` - ⚠️
+    - `InfoUserFeedbackEmoji` - 📨
+    - `DefaultUserFeedbackEmoji` - 🔍
+
+    These are linked to the `ExceptionLevel` enum.
+
+    +----------------+-----------------+
+    | ExceptionLevel | EmojiStrategy   |
+    +================+=================+
+    | ERROR          | 1               |
+    +----------------+-----------------+
+    | WARNING        | 2               |
+    +----------------+-----------------+
+    | INFO           | 3               |
+    +----------------+-----------------+
+    | DEFAULT        | Any other value |
+    +----------------+-----------------+
+    """
+    def __init__(
+        self,
+        message: str,
+        emoji_strategy: UserFeedbackEmojiStrategy,
+        formatters: tuple[Callable[[str], str], ...] = (),
+    ) -> None:
         self.message: str = message
-        self.emoji_strategy = emoji_strategy
+        self.emoji_strategy: UserFeedbackEmojiStrategy = emoji_strategy
+        self.formatters: tuple[Callable[[str], str], ...] = formatters
         super().__init__(message)
 
     def __str__(self) -> str:
-        return f"{self.emoji_strategy.get_emoji()} | {self.message}"
+        formatted_message: str = self.message
+        for formatter in self.formatters:
+            formatted_message = formatter(formatted_message)
+        return f"{self.emoji_strategy.get_emoji()} | {formatted_message}"
 
 
 class UserFeedbackEmojiStrategy(ABC):
@@ -33,7 +80,7 @@ class UserFeedbackEmojiStrategy(ABC):
 
 class DefaultUserFeedbackEmoji(UserFeedbackEmojiStrategy):
     def get_emoji(self) -> str:
-        return "🔶"
+        return "🔍"
 
 
 class ErrorUserFeedbackEmoji(UserFeedbackEmojiStrategy):
@@ -48,7 +95,7 @@ class WarningUserFeedbackEmoji(UserFeedbackEmojiStrategy):
 
 class InfoUserFeedbackEmoji(UserFeedbackEmojiStrategy):
     def get_emoji(self) -> str:
-        return "🔮"
+        return "📨"
 
 
 class UserFeedbackExceptionFactory:
@@ -59,7 +106,13 @@ class UserFeedbackExceptionFactory:
     }
 
     @staticmethod
-    def create(message: str, level: ExceptionLevel = ExceptionLevel.ERROR) -> UserFeedbackException:
+    def create(
+        message: str,
+        level: ExceptionLevel = ExceptionLevel.ERROR,
+        formatters: tuple[Callable[[str], str], ...] = (),
+    ) -> UserFeedbackException:
         return UserFeedbackException(
-            message, UserFeedbackExceptionFactory.EMOJI_STRATEGIES.get(level, DefaultUserFeedbackEmoji())
+            message,
+            UserFeedbackExceptionFactory.EMOJI_STRATEGIES.get(level, DefaultUserFeedbackEmoji()),
+            formatters,
         )
